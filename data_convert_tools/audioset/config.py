b8ccf26092e48babfcb23686e4d14c08d72e4707
yuliangzhang <yuliangzhang0030@gmail.com>
2025-01-17 07:24:14 +0800

    202501170712: update the audioset convert tools

--- data_convert_tools/audioset/config.py
+++ data_convert_tools/audioset/config.py
@@ -5,7 +5,7 @@ sample_rate = 32000
 clip_samples = sample_rate * 10     # Audio clips are 10-second
 
 # Load label
-with open('metadata/class_labels_indices.csv', 'r') as f:
+with open('class_labels_indices.csv', 'r') as f:
     reader = csv.reader(f, delimiter=',')
     lines = list(reader)
 
